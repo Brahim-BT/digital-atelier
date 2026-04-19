521735b429daf7cf39038c0c5ef5a9fe644f0d16
Brahim-BT <baibbat.brahim@gmail.com>
2026-04-19 21:16:54 +0100

    Add PrimeNG themes and update app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import Aura from '@primeuix/themes/aura';
 
 export const AppConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
